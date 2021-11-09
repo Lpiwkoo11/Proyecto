40d4d1713d32099e1351ac0abd4ed3970f4cee6d
Lucas <lpiwko@udesa.edu.ar>
2021-11-09 19:41:45 -0300

    agergando logo de the movie data al footer y empezando formulario en el header

---
+++ js/home.js
@@ -0,0 +1,12 @@
+// Header (buscador)
+
+let caracteres = 3;
+let acceso = '';
+
+if (caracteres < 3) {
+    acceso = 'prohibido';
+} else {
+    acceso = 'permitido';
+}
+
+console.log(caracteres);
