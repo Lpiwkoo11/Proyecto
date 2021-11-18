a4782df9c079afcf65088fc88c1f37605cc7b3e2
martinlotz <mlotz@udesa.edu.ar>
2021-11-18 18:56:13 -0300

    resultado de busqueda en java falta terminar

--- js/search-results.js
+++ js/search-results.js
@@ -1,4 +1,33 @@
 window.addEventListener('load', function(){
-  
+
+    // Acceder a la Query String
     
-})
+    let queryString = location.search;
+    let objetoQueryString = new URLSearchParams(queryString);
+    let busqueda = objetoQueryString.get('.buscador');
+    console.log(buscador);
+    
+    // fetch
+    
+    fetch(`https://api.themoviedb.org/3/movie/550?api_key=8316164d28e6834ac04fce13a384de41`)
+        .then(function(response){
+            return response.json();
+        }).then(function(datos){
+            
+            console.log(datos);
+    
+            for(let i = 0; i < datos.data.length; i++){
+                document.querySelector('section').innerHTML += `
+                    <article>
+                        <h5>${datos.data[i].title}</h5>
+                        <div><img src="${datos.data[i].images.original.url}" alt="Imagen"></div>
+                        <a href="detalle.html?id=${datos.data[i].id}">Ver más información</a>
+                    </article>
+                `;
+            }
+    
+        }).catch(function(error){
+            console.log(`El error fue: ${error}`);
+        })
+    
+    })
