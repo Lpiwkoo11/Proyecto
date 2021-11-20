00e9d4972d436a5dcd2e133404e9285ff23e123a
Benjamin <bpena@udesa.edu.ar>
2021-11-20 15:26:51 -0300

    Correccion  del js y html de generos y detalle de generos

--- js/detallegenero.js
+++ js/detallegenero.js
@@ -1,51 +1,55 @@
+// Acceso Query String
 let queryString = location.search;
-    let objetoQueryString = new URLSearchParams(queryString);
-    let busqueda = objetoQueryString.get('.Comedia');
-    console.log(Comedia);
+let objetoQueryString = new URLSearchParams(queryString);
+let id = objetoQueryString.get('idGenero');
+console.log(id);
+let genero = objetoQueryString.get(`genero`);
+console.log(genero);
 
-fetch(`https://api.themoviedb.org/3/genre/movie/list?api_key=20342717cddddd7790a3d41e00d1854d`)
-    .then(function(response){
-        return response.json();
-    })
-    .then(function(datos){
-
-        console.log(datos.results);
-
-        for(let i = 0; i < 5; i++){
-            document.querySelector('.Comedia').innerHTML += `
-            <article>
-                <div>
-                <img src="https://image.tmdb.org/t/p/w342${datos.results[i].poster_path}" alt="pelicula">
-                </div>
-                <h5>${datos.results[i].title}</h5>
-                <a href="detallepelicula.html?id=${datos.results[i].id}">Ver mas informacion</a>
-            </article>
-            `;
-        };
-    })
-
-    fetch(`https://api.themoviedb.org/3/genre/tv/list?api_key=20342717cddddd7790a3d41e00d1854d`)
-    .then(function(response){
-        return response.json();
-    })
-    .then(function(datos){
-
-        console.log(datos.results);
-
-        for(let i = 0; i < 5; i++){
-            document.querySelector('.Comedia').innerHTML += `
-            <article>
-                <div>
-                <img src="https://image.tmdb.org/t/p/w342${datos.results[i].poster_path}" alt="pelicula">
-                </div>
-                <h5>${datos.results[i].title}</h5>
-                <a href="detallepelicula.html?id=${datos.results[i].id}">Ver mas informacion</a>
-            </article>
-            `;
-        };
-    })
-
-    .catch(function(error){
-        console.log("error:" + error);
-    })
-
+// Fetch
+fetch(`https://api.themoviedb.org/3/discover/movie?api_key=20342717cddddd7790a3d41e00d1854d&language=es&with_genres=${id}`)
+   .then(function(response){
+       return response.json();
+   })
+   .then(function(datos){
+ 
+       console.log(datos);
+       console.log(datos.genres);
+ 
+       for(let i = 0; i < 5; i++){
+           document.querySelector('.detalle-genero').innerHTML += `
+           <article>
+               <div>
+               <img src="https://image.tmdb.org/t/p/w342${datos.results[i].poster_path}" alt="pelicula">
+               </div>
+               <h5>${datos.results[i].title}</h5>
+               <a href="detallepelicula.html?id=${datos.results[i].id}">Ver mas informacion</a>
+           </article>
+           `;
+       };
+   })
+   fetch(`https://api.themoviedb.org/3/discover/movie?api_key=20342717cddddd7790a3d41e00d1854d&language=es&with_genres=${id}`)
+   .then(function(response){
+       return response.json();
+   })
+   .then(function(datos){
+ 
+       console.log(datos);
+       console.log(datos.genres);
+ 
+       for(let i = 0; i < 5; i++){
+           document.querySelector('.detalle-genero').innerHTML += `
+           <article>
+               <div>
+               <img src="https://image.tmdb.org/t/p/w342${datos.results[i].poster_path}" alt="pelicula">
+               </div>
+               <h5>${datos.results[i].title}</h5>
+               <a href="detallepelicula.html?id=${datos.results[i].id}">Ver mas informacion</a>
+           </article>
+           `;
+       };
+   })
+ 
+   .catch(function(error){
+       console.log("error:" + error);
+   })
--- js/generos.js
+++ js/generos.js
@@ -1,39 +1,25 @@
-fetch(`https://api.themoviedb.org/3/genre/movie/list?api_key=f2acabc2f1f7dfa29f6493c2fcca003f`)
-    .then(function(response){
-        return response.json();
-    })
-    .then(function(datos){
+// Fetch
+fetch(`https://api.themoviedb.org/3/genre/movie/list?api_key=f2acabc2f1f7dfa29f6493c2fcca003f&language=es?`)
+   .then(function(response){
+       return response.json();
+   })
+   .then(function(datos){
+ 
+       console.log(datos.genres);
+ 
+ 
+       for(let i = 0; i < 5; i++){
+           document.querySelector('.generos-peliculas').innerHTML += `
+ 
+           <article class="peliculas-art">
+               <div class="peliculas-div">
+               </div>
+                   <a href="detalledelgenero.html?idGenero=${datos.genres[i].id}&genero=${datos.genres[i].name}">${datos.genres[i].name}</a>
+           </article>
+           `;
+       };
+   })
 
-        console.log(datos.results);
-
-        for(let i = 0; i < 5; i++){
-            document.querySelector('.generos-peliculas').innerHTML += `
-
-            <article class="peliculas-art">
-                <div class="peliculas-div">
-                    <img src="https://image.tmdb.org/t/p/w342${datos.results[i].poster_path}" alt="imagen comedia">
-                </div>
-                    <a href="detalledelgenero.html?=${datos.results[i].id}">Comedia</a>
-            </article>
-
-            <article class="peliculas-art">
-                <div class="peliculas-div">
-                    <img src="https://image.tmdb.org/t/p/w342${datos.results[i].poster_path}" alt="imagen suspenso">
-                </div>
-                    <a href="detalledelgenero.html?=${datos.results[i].id}">Suspenso</a>
-            </article>
-
-            <article class="peliculas-art">
-                <div class="peliculas-div">
-                    <img src="https://image.tmdb.org/t/p/w342${datos.results[i].poster_path}" alt="imagen ficción">
-                </div>
-                    <a href="detalledelgenero.html?=${datos.results[i].id}">Drama</a>
-            </article>
-
-            `;
-        };
-        
-    })
-    .catch(function(error){
-        console.log("error:" + error);
-    })
+   .catch(function(error){
+       console.log("error:" + error);
+   })
